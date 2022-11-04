6cdc8edcc8b60c0916231b943394bd59c77d1ea3
michal_oles <olesmichalxp@gmail.com>
2022-11-04 16:03:53 +0100

    Eratosthenes Sieve

--- go/simple/functions/Function.go
+++ go/simple/functions/Function.go
@@ -28,3 +28,29 @@ func Fibonacci(n int) int {
 	}
 	return result
 }
+
+func EratosthenesSieve(n int) []int {
+	numbersNoFirst := make([]bool, n)
+	result := make([]int, 0)
+	if n < 1 {
+		return result
+	}
+	numbersNoFirst[0] = true
+	for i := 2; i <= n; i++ {
+		if numbersNoFirst[i-1] {
+			continue
+		}
+		j := 2
+		for i*j <= n {
+			numbersNoFirst[(i*j)-1] = true
+			j++
+		}
+	}
+
+	for i := 0; i < n; i++ {
+		if !numbersNoFirst[i] {
+			result = append(result, i+1)
+		}
+	}
+	return result[:]
+}
--- go/simple/main.go
+++ go/simple/main.go
@@ -11,10 +11,14 @@ func main() {
 	var a int = 1
 	var b int = 2
 	var n int = 5
+	var nn int = 20
 	fmt.Println("Hello, World!")
 	fmt.Printf("%d + %d = %d\n", a, b, f.Add(a, b))
 	fmt.Printf("Factor n=%d is %d\n", n, f.Factor(n))
 	fmt.Printf("Fibonacci sequence n=%d is %d\n", n, f.Fibonacci(n))
+	fmt.Println("--------------------------")
+	fmt.Printf("Eratosthenes Sieve n=%d is :\n", nn)
+	fmt.Println(f.EratosthenesSieve(nn))
 	fmt.Println("--------------------------")
 	example.Write()
 
