4935954932bedfbee8a903ff65debc20b8f2d1f3
michal_oles <olesmichalxp@gmail.com>
2022-12-03 18:16:16 +0100

    Go chart

---
+++ go/chart/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"math/rand"
+	"os"
+
+	"github.com/go-echarts/go-echarts/v2/charts"
+	"github.com/go-echarts/go-echarts/v2/opts"
+)
+
+func generateBarItems() []opts.BarData {
+	items := make([]opts.BarData, 0)
+	for i := 0; i < 7; i++ {
+		items = append(items, opts.BarData{Value: rand.Intn(300)})
+	}
+	return items
+}
+
+func main() {
+	bar := charts.NewBar()
+	bar.SetGlobalOptions(charts.WithTitleOpts(opts.Title{
+		Title:    "Chart",
+		Subtitle: "Test",
+	}))
+
+	// Put data into instance
+	bar.SetXAxis([]string{"Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"}).
+		AddSeries("Category A", generateBarItems()).
+		AddSeries("Category B", generateBarItems())
+	// Create chart by js
+	f, _ := os.Create("bar.html")
+	bar.Render(f)
+}
